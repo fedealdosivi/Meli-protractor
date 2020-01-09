b2804b927b64bb5a9e4f482b34d1de7fc90fdee2
Federico Palomero <fedepalo13@ggmail.com>
2020-01-09 17:42:57 -0300

    fixed angular sync

--- todo-spec.js
+++ todo-spec.js
@@ -1,13 +1,21 @@
 describe('angularjs homepage todo list', function() {
 
 
-    const searchBar = element(by.className(''));
+    const searchBar = element(by.className('nav-search-input'));
+    const searchBtn = element(by.className('nav-search-btn'));
+    const searchResults = element(by.id('searchResults'));
+    const EC = protractor.ExpectedConditions;
 
     beforeEach(function() {
+        browser.ignoreSynchronization = true;
+        browser.waitForAngularEnabled(false);
+        browser.driver.manage().window().maximize();
         browser.get('https://www.mercadolibre.com.ar/');
     });
 
     it('Validate searching', function() {
-        searchBar.sendKeys('leggings');
+        searchBar.sendKeys('Aldosivi');
+        searchBtn.click();
+        browser.wait(EC.visibilityOf(searchResults), 5000);
     });
 });
